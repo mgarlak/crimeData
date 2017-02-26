8cfec1b556987057854a0681074f2903348c7051
mgarlak <mgarlak@kent.edu>
2017-02-25 22:48:03 -0500

    Add files via upload
    Compiling run.py executes a handful bash commands.  My school server wouldn't let me execute a bash script, but I could run a python script.  Therefore, I just put the necessary bash commands in the python script.

---
+++ run.py
@@ -0,0 +1,10 @@
+## Michael Garlak
+## 02-25-2017
+## Testing Python 
+
+import os
+
+os.system('curl -o page.html http://louisville.edu/police/crime-log/february-2017-crime-log')
+os.system('g++ crime2database.cpp -o crime2database -std=c++11 -l sqlite3')
+os.system('./crime2database')
+os.system('rm crime2database page.html')
